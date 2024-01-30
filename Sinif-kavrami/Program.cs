86d5fbb4d09d9b845ed487a8d7ea1c8a5a66532b
Cenk Kıran <cenk33m@gmail.com>
2024-01-30 16:01:40 +0300

    First commit

---
+++ Sinif-kavrami/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+
+        Employee employee= new Employee(){Id = 1, Name = "cenk",LastName = "kıran", Departman="Physic"};
+        employee.CallInformation();
+    }
+}
+
+class Employee{
+    public Employee(string ad ,string soyad, int no , string departman)
+    {
+        Id = no;
+        Name = ad;
+        LastName = soyad;
+        Departman = departman;
+    }
+    public Employee()
+    {
+        
+    }
+    public int Id { get; set;}  
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Departman { get; set; }
+
+    public void CallInformation(){
+        Console.WriteLine($"id : {Id}");
+         Console.WriteLine($"name: {Name}");
+          Console.WriteLine($"surname : {LastName}");
+           Console.WriteLine($"Departman : {Departman}");
+           
+
+    }
+}
