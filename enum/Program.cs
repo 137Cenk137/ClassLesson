6013d7191ef1256b0879ee64baa60b8d4930cb7b
Cenk Kıran <cenk33m@gmail.com>
2024-02-01 18:16:59 +0300

    enum code was written

---
+++ enum/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Günler.Pazar);
+        Console.WriteLine(Convert.ToInt32 (Günler.Pazar.));
+
+        int sicaklis = 32;
+        if (((int)HavaDurumu.Sıcak) <=sicaklis )
+        {
+            
+        }
+
+    }
+}
+
+enum Günler{
+    Pazartesi,
+    Sali,
+    Carsamba,
+    Persembe,
+    Cuma,
+    Pazar
+}
+
+enum HavaDurumu{
+    Soguk = 5,
+    Normal = 20,
+    Sıcak = 30
+}
