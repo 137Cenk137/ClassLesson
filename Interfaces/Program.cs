0acff2e36a124ebfa3419c5f86bb46af7af8b7b6
Cenk Kıran <cenk33m@gmail.com>
2024-02-03 21:14:23 +0300

    interfaces was learned

---
+++ Interfaces/Program.cs
@@ -0,0 +1,57 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FileLogger fileLogger= new FileLogger();
+        fileLogger.Writelog();
+        DatabaseLogger databaseLogger= new DatabaseLogger();
+        databaseLogger.Writelog();
+        SMSLOgger sMSLOgger= new SMSLOgger();
+        sMSLOgger.Writelog();
+
+        LogManager logManager= new LogManager(new FileLogger());
+        logManager.Writelog(); 
+    }
+}
+
+interface ILogger
+{
+    void Writelog();
+}
+
+class LogManager{
+    private ILogger _logger;
+
+    public LogManager(ILogger logger)
+    {
+       _logger = logger;   
+    }
+
+    public void Writelog()
+    {
+        _logger.Writelog();
+    }
+}
+
+class DatabaseLogger : ILogger
+{
+    public void Writelog()
+    {
+         Console.WriteLine("Database e  loglama yapar");
+    }
+}
+class FileLogger : ILogger
+{
+    public void Writelog()
+    {
+         Console.WriteLine("dosyaya   loglama yapar");
+    }
+}
+
+class SMSLOgger : ILogger
+{
+    public void Writelog()
+    {
+        Console.WriteLine("SMS olarak loglama yapar");
+    }
+}
