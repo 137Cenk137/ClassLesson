1a4657a87e029e1edd0b6b915d8743e1bea71eb0
Cenk Kıran <cenk33m@gmail.com>
2024-02-17 19:27:46 +0300

    circle

---
+++ circle/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DrawCircle.Circle();
+    }
+}
+public class DrawCircle{
+    public static void Circle(){
+        Console.Write("yarıcapi giriniz: ");
+        int lengthOfCircle = Convert.ToInt32(Console.ReadLine());  
+
+        for (int i = 1; i < lengthOfCircle*2; i++)//y
+        {
+            for (int j = 1;j < lengthOfCircle*2; j++){//x
+
+                if (lengthOfCircle<j && lengthOfCircle+i>j){
+                    Console.Write("*");
+                }
+                else{
+                    Console.Write("-");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
