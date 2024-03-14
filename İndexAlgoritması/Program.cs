bf43886d581f98f186437de40834e81864a2ec39
Cenk Kıran <cenk33m@gmail.com>
2024-03-14 22:42:10 +0300

    index app was completed

---
+++ İndexAlgoritması/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        var tempList = new List<string>();
+        Console.WriteLine("kelimeri virgül ile ayıran cümleciler girin");//enter clauses separating words with commas
+        var  sentece = Console.ReadLine();
+        foreach (var item in sentece){
+            if(char.IsNumber(item) || item == ','){}
+            else{
+                tempList.Add(Convert.ToString(item));
+            }
+
+        }
+
+        foreach (var item in tempList){
+            Console.Write(item);
+            
+
+        }
+
+        
+    }
+        
+    
+}
