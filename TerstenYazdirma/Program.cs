1936c47611c0c5f597fb05d07712d3673ce203d4
Cenk Kıran <cenk33m@gmail.com>
2024-03-15 10:09:40 +0300

    reverse prosses was completed

---
+++ TerstenYazdirma/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("enter clauses please (for reverse sentence)");
+        string sentence = Console.ReadLine();
+        for (int i = sentence.Length-1; i >=  0 ; i--)
+        {
+            Console.Write(sentence[i]);
+
+    
+        } 
+        Console.WriteLine( );
+    }
+}
